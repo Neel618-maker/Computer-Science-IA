e3e786ba551d7fb611b71a8264891e99508123b9
Neel-618-maker <neel.saney@online.island.edu.hk>
2025-09-19 09:07:55 +0800

    j

---  main.py
+++  main.py
@@ -7,7 +7,7 @@ from utils import calculate_progress
 
 init_db()
 
-# Register and login
+# Register and login hello
 register("neel@example.com", "securepass123")
 user_id = login("neel@example.com", "securepass123")
 
