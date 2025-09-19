288983f10fc3f2aa1ef42de74529b42f4ac7a9ab
Neel-618-maker <neel.saney@online.island.edu.hk>
2025-09-19 10:19:47 +0800

    IA day 3

--- database.py
+++ database.py
@@ -4,33 +4,45 @@ def init_db():
     conn = sqlite3.connect("fitness.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+    cursor.execute('''
+      CREATE TABLE IF NOT EXISTS users(
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE NOT NULL,
-        password_hash TEXT NOT NULL
-    )""")
-
-    cursor.execute("""
-CREATE TABLE IF NOT EXISTS workout_sets (
-    set_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    exercise TEXT,
-    reps INTEGER,
-    weight_kg REAL,
-    is_bodyweight INTEGER DEFAULT 0,
-    date TEXT
-)""")
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS goals (
-        goal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+        house TEXT
+        student_id TEXT UNIQUE
+        password TEXT NOT NULL
+        exercise_id TEXT,
+        created_at TEXT DEFAULT CURRENT TIMESTAMP
+    )
+    ''')    
+    
+    cursor.execute('''
+      CREATE TABLE IF NOT EXISTS workout_sets(
         user_id INTEGER,
-        exercise TEXT,
-        target_reps INTEGER,
+        exerciese TEXT,
+        reps INTEGER,
+        weight REAL
+        is_bodyweight INTEGER,
+        datetime TEXT DEFAULT CURRENT TIMESTAMP,
+    )                                                                                                                                   
+ ''')
+    
+    cursor.execute(''')
+      CREATE TABLE IF NOT EXISTS goals(
+        user_id INTEGER,
+        exercise TEXT,  
+        target_reps INTEGER
         target_weight REAL,
-        deadline TEXT
-    )""")
-   
+    )
+ ''')
+
     conn.commit()
     conn.close()
+    def add_user(name, house, student_id, password):
+        conn = sqlite3.connect("fitness.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO users (name, house, student_id, password)
+                Values (?, ?, ?, ?)
+                       """, (name, house, student_id, password))
