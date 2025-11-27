9dbc85e377356ef09bea6d3aa3f83a020ebb8da0
Neel-618-maker <neel.saney@online.island.edu.hk>
2025-11-27 10:03:09 +0800

    why not more

--- add_users.py
+++ add_users.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("fitness.db")
 cursor = conn.cursor()
 
-users = [("Neel", "mypassword123"), ("Alex", "securepass456"), ("Maya", "workout789"), ("Ravi", "fitnesslover"),]
+users = [("John", "three123"), ("Jamies", "Steve123"), ("Ashton", "crazy123"), ("Jamie", "king123"),]
 cursor.executemany("INSERT INTO users (name, password) VALUES (?, ?)", users)
 conn.commit()
 conn.close()
